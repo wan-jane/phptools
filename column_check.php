50bc36de4b5bccc829fb97a92be2fd40a902b897
wawa <gengkang@xnw.com>
2017-05-06 10:41:50 +0800

    检查所有的表里是否有某个字段

---
+++ column_check.php
@@ -0,0 +1,23 @@
+<?php
+$pdo = new PDO("mysql:host=127.0.0.1;dbname=test;charset=utf8;",'root','root');
+$column = 'delete_time';
+$sql = 'show tables';
+$tables = [];
+$st = $pdo->query($sql);
+$tables = $st->fetchAll(PDO::FETCH_NUM);
+
+$tables = array_column($tables, 0);
+$dt = [];
+$dt_sql = [];
+foreach($tables as $table) {
+    $sql = sprintf("Describe %s `%s`", $table, $column);
+    $st = $pdo->query($sql);
+    $result = $st->fetch();
+    if ($result == false) {
+        echo $table . " 没有指定字段\n";
+        $dt[] = $table;
+        $dt_sql[] = sprintf("ALTER TABLE `%s` ADD COLUMN `%s` BIGINT(20) NULL DEFAULT NULL COMMENT '软删除时间';", $table, $column);
+    }
+}
+print_r($dt);
+print_r($dt_sql);
